06c2834ca4027b2c119ea8fbde6a26fdc678f4c0
denis96z <zinoviev.denis@yandex.ru>
2018-12-20 21:48:20 +0300

    error fixed

--- decrypt.sh
+++ decrypt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo ${DATA_PASSWORD} | gpg -d -o args.tar.gz args.tar.gz.gpg
-tar -xzf args.tar.gz
-rm args.tar.gz
+dir="$1"
+
+echo ${DATA_PASSWORD} | gpg -d -o ${dir}/args.tar.gz \
+        ${dir}/args.tar.gz.gpg &&
+    tar -xzf ${dir}/args.tar.gz &&
+    rm ${dir}/args.tar.gz
