af0b4f8634cabebbeb479e0b9fbf8007b1c34854
denis96z <zinoviev.denis@yandex.ru>
2018-12-21 03:10:49 +0300

    deploy added

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export DOCKER_TLS_VERIFY=1
+export COMPOSE_TLS_VERSION=TLSv1_2
+export DOCKER_CERT_PATH=${TRAVIS_BUILD_DIR}
+export DOCKER_HOST=tcp://${SERVER_HOST}:${SERVER_PORT}
+
+./service-config/decrypt.sh . . &&
+  docker-compose pull && docker-compose up -d
